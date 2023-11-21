d0f98f8ca7ef7fa05244875536460c7b7f6d9881
gab0-21 <bruno_gaborever@hotmail.com>
2023-11-21 08:42:30 -0600

    Cambio de nombre y matricula

---
+++ hola_mundo.py
@@ -0,0 +1,7 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
+
+if __name__ == "__main__":
+
+    saludar("Bruno Gabriel Oropeza Hernandez", "2223028637")
